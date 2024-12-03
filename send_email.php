215fe9d3e5d52ae0878031207eeff828612195fd
Mauzge <maurin12@bluewin.ch>
2024-12-03 08:20:03 +0100

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+
+    if ($email) {
+        $to = "maurin12@bluewin.ch";
+        $subject = "Neue Adventskalender-Teilnahme";
+        $message = "Die folgende E-Mail-Adresse hat am Adventskalender teilgenommen:\n\n$email";
+        $headers = "From: no-reply@adventskalender2024.com";
+
+        if (mail($to, $subject, $message, $headers)) {
+            echo "Ihre E-Mail-Adresse wurde erfolgreich übermittelt.";
+        } else {
+            echo "Es gab ein Problem beim Senden der E-Mail. Bitte versuchen Sie es später erneut.";
+        }
+    } else {
+        echo "Ungültige E-Mail-Adresse.";
+    }
+} else {
+    echo "Ungültige Anfrage.";
+}
+?>
